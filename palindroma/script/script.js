c2828fb9606055a88f1629145dda05e9dec64800
Alessandro Montenero <alemonte99@icloud.com>
2023-01-31 16:02:31 +0100

    palindroma

--- palindroma/script/script.js
+++ palindroma/script/script.js
@@ -5,14 +5,19 @@ Creare una funzione per capire se la parola inserita è palindroma */
 /* onclick */
 document.getElementById("button").onclick = function() {
     let wordToCheck = document.getElementById("word").value
-    console.log(palindroma(wordToCheck))
+    palindroma(wordToCheck)
 }
 
 function palindroma(word) {
+    document.getElementById("checkWord").innerHTML = ""
+    document.getElementById("printWord").innerHTML = ""
+
     /* dichiaro un array per la parola inserita*/
     let wordArray = []
     /* dichiaro un array per la parola reverse */
     let reverseArray = []
+    /* dichiaro una variabile di verifica per salvare se la parola è palindroma */
+    checkVerify = 0
 
     /* salvo la sua lunghezza */
     wordLength = word.length
@@ -21,19 +26,29 @@ function palindroma(word) {
     for (i = 0; i < wordLength; i++) {
         wordArray[i] = word[i]
         reverseArray[(wordLength - 1) - i] = word[i]
-        console.log(wordArray)
-        console.log(reverseArray)
     }
 
-    /* controllo in base alla lunghezza della parola se i valori dei due array coincidono */
+    /* controllo in base alla lunghezza della parola se i valori dei due array coincidono e stampo la parola reverse */
     let checkArray = []
     for (i = 0; i < wordLength; i++)
     if (wordArray[i] == reverseArray[i]) {
         checkArray[i] = true
+        document.getElementById("printWord").innerHTML += '<p>' + wordArray[i] + "</p>"
+        document.getElementById("checkWord").innerHTML += '<p class="green">' + wordArray[i] + "</p>"
     }
     else {
         checkArray[i] = false
-        return false
+        document.getElementById("printWord").innerHTML += '<p>' + wordArray[i] + "</p>"
+        document.getElementById("checkWord").innerHTML += '<p class="red">' + wordArray[i] + "</p>"
+        checkVerify += 1
     }
-    console.log(checkArray)
+
+    if (checkVerify != 0){
+        document.getElementById("checkResult").innerHTML = "La parola non è palindroma."
+    }
+
+    else {
+        document.getElementById("checkResult").innerHTML = "La parola è palindroma."
+    }
+    
 }
